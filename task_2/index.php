9365917e0cef50121185ff9758a72dfabe0f8af0
cocenage <89825630221@mail.ru>
2024-01-17 16:57:48 +0300

    задание один

--- task_2/index.php
+++ task_2/index.php
@@ -1,9 +1,17 @@
 <?
-$dom = 23;
-if($dom >= 18) {
-    echo "boys dont cry";
-} elseif{
-    echo "boys cry ";
-} else {
+$dom = 4;
 
+
+
+
+if($dom == 1) {
+    echo "идет дождь";
+} else if ($dom == 2){
+    echo "идет снег ";
+} else if ($dom == 3){
+    echo "тепло ";
+} 
+
+else {
+    echo "ничего не идет ";
 }
