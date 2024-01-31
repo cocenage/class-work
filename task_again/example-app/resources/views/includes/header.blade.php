865863555ad80d9ad9b9798d8e12ece7e7986386
cocenage <89825630221@mail.ru>
2024-01-31 19:53:13 +0300

    лютый сайт

---
+++ task_again/example-app/resources/views/includes/header.blade.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <div class="header__container">
+        <div class="header__inner">
+            <ul class="header__items">
+                <li class="header__item">
+                    <a href="{{ route('home') }}" class="header__link">{{__('главная')}}</a>
+                </li>
+                <li class="header__item">
+                    <a href="" class="header__link">{{__('О нас')}}</a>
+                </li>
+                <li class="header__item">
+                    <a href="" class="header__link">{{__('Все записи')}}</a>
+                </li>
+                <li class="header__item">
+                    <a href="" class="header__link">{{__('Все посты')}}</a>
+                </li>
+                <li class="header__item">
+                    <a href="" class="header__link">{{__('Все категори')}}</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</header>
---
+++ task_again/example-app/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="container">
+        <h1>Страница кириешки</h1>
+
+        <div class="block">
+            <img src=https://avatars.mds.yandex.net/i?id=cc74c52a41e896a0beab590450e8f869-5220825-images-thumbs&ref=rim&n=33&w=287&h=200" alt="" class="img">
+            <div class="descr">
+                <h3 class="name">Кириешка Лисицын</h3>
+                <p class="description">Привет, Я кириешка киттебойс, я непосредственный чат бот, который готов с тобой общаться </p>
+            </div>
+        </div>
+    </div>
+
+@endsection
